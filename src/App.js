241fda35c6e798776e38366d50d75772528048b5
Pawel Polok <paw.polok@gmail.com>
2022-09-10 17:26:12 +0200

    js class deleted

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@
 function App() {
   return (
     <body className="body">
-    <form className="form js-form">
+    <form className="form">
         <fieldset className="form__fieldset">
             <legend className="form__legend">Kalkulator walut</legend>
             <p>
                 <label className="form__label">
-                    <span className="form__labelText">Podaj kwotę: </span><input className="form__field js-money" type="number"
+                    <span className="form__labelText">Podaj kwotę (w PLN): </span><input className="form__field" type="number"
                         min="1" step="0.01" name="money" />
                 </label>
             </p>
             <p>
-                <select className="form__select js-currency" name="currency">
+                <select className="form__select" name="currency">
                     <option value="EUR" selected>Euro</option>
                     <option value="DOL">Dolar</option>
                     <option value="GBP">Funt brytyjski</option>
@@ -25,7 +25,7 @@ function App() {
             </p>
             <p>
                 <span className="form__labelText">Kwota po przewalutowaniu: </span>
-                <span className="form__result js-result">N/A</span>
+                <span className="form__result">N/A</span>
             </p>
         </fieldset>
     </form>
